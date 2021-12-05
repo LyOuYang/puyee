142a694da19989029e566c23e3208f25fc4897a3
Dehong <1252825428@qq.com>
2021-12-05 19:48:52 +0800

    system picture recognition

--- app/src/main/java/com/example/puyee/ImageActivity.java
+++ app/src/main/java/com/example/puyee/ImageActivity.java
@@ -11,6 +11,9 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
@@ -42,7 +45,39 @@ public class ImageActivity extends BaseActivity {
     ImageView imageView;
     Button confirm;
     RecognizeRsp result;
-    Handler handler;
+    Handler handler = new Handler(Looper.myLooper()) {
+        @Override
+        public void dispatchMessage(@NonNull Message msg) {
+            super.dispatchMessage(msg);
+            if (msg.what == 1) {
+                List<List<Double>> box = result.getDetection_boxes();
+                Bitmap bitmap = ((BitmapDrawable)imageView.getDrawable()).getBitmap();
+                Canvas canvas = new Canvas(bitmap);
+                Paint paint = new Paint();
+                // 抗锯齿
+                paint.setAntiAlias(true);
+                // 防抖动
+                paint.setDither(true);
+
+                paint.setTextSize(39);
+                paint.setColor(Color.parseColor("#ff0000"));
+//                canvas.drawText(text, x, y, paint);
+//                return bitmap;
+            }
+        } };
+
+    Runnable r1 = new Runnable() {
+        @Override
+        public void run() {
+            Bitmap bitmap = ((BitmapDrawable)imageView.getDrawable()).getBitmap();
+            byte[] bytes = NetworkUtils.bitmapToByte(bitmap, Bitmap.CompressFormat.PNG);
+            result = NetworkUtils.getPuyeeRecognize(bytes);
+            Message message = new Message();
+            message.what = 1;
+            handler.sendMessage(message);
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,31 +96,13 @@ public class ImageActivity extends BaseActivity {
             in.setType("image/*");
             startActivityForResult(in, 2);
         }
-        handler = new Handler(Looper.myLooper()) {
-            @Override
-            public void dispatchMessage(@NonNull Message msg) {
-                super.dispatchMessage(msg);
-                if (msg.what == 1) {
-                    List<List<Double>> box = result.getDetection_boxes();
-                }
-            } };
+
         confirm = findViewById(R.id.confirm);
         confirm.setOnClickListener((v) -> {
             Thread thread = new Thread(r1);
             thread.start();
         });
     }
-        Runnable r1 = new Runnable() {
-            @Override
-            public void run() {
-                Bitmap bitmap = imageView.getDrawingCache();
-                byte[] bytes = NetworkUtils.bitmapToByte(bitmap, Bitmap.CompressFormat.PNG);
-                result = NetworkUtils.getPuyeeRecognize(bytes);
-                Message message = new Message();
-                message.what = 1;
-                handler.sendMessage(message);
-            }
-        };
 
     private boolean isGetPermission() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED &&
